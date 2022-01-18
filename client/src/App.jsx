05b61ead7596dbef993937a2fd8230eb39546716
Dan Prudhomme <dannyprud@yahoo.com>
2022-01-17 20:28:09 -0600

    Create React App, remove unused files

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'
+
+const App = () => {
+    return ( 
+        <div>Dan Prudhomme Jr</div>
+     );
+}
+ 
+export default App;
