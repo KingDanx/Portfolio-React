ae43259a8a55197b69df7804fd7beadc64fd2f1a
Dan Prudhomme <dannyprud@yahoo.com>
2022-01-22 23:33:18 -0600

    add extra icons, place self in center with icons rotating around my image

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import resume from "../../../resume/Dan Prudhomme - dev Resume.docx";
 import dan from "../../../images/Dan-Prudhomme.jpg";
+import js from "../../../images/javascript.png";
+import html from "../../../images/HTML.png";
+import css from "../../../images/CSS.3.svg";
+import react from "../../../images/react.png";
+import node from "../../../images/node.png";
 import Button from "@mui/material/Button";
 import "./styles/Home.css";
-import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+
 
 const Home = () => {
   return (
@@ -11,17 +16,48 @@ const Home = () => {
       Home
       <h1>Dan Prudhomme Jr</h1>
       <div>
-        <img src={dan} alt="image of Dan Prudhomme Jr" />
+        
         <div className="main">
+        <img src={dan} alt="image of Dan Prudhomme Jr" className="me"/>
           <div className="item">
             <img
-              src={dan}
+              src={js}
               height="50px"
-              style={{ borderRadius: "50%" }}
-              alt="image of Dan Prudhomme Jr"
+              alt="image of JavaScript"
             />
           </div>
-          <div className="item"><AirplanemodeActiveIcon/></div>
+          <div className="item" style={{animationDelay: "-3s"}}>
+            <img
+              src={css}
+              height="50px"
+              alt="image of CSS icon"
+              style={{animationDelay: "-3s"}}
+            />
+          </div>
+          <div className="item" style={{animationDelay: "-6s"}}>
+            <img
+              src={html}
+              height="50px"
+              alt="image of HTML icon"
+              style={{animationDelay: "-6s"}}
+            />
+          </div>
+          <div className="item" style={{animationDelay: "-9s"}}>
+            <img
+              src={react}
+              height="50px"
+              alt="image of react icon"
+              style={{animationDelay: "-9s"}}
+            />
+          </div>
+          <div className="item" style={{animationDelay: "-12s"}}>
+            <img
+              src={node}
+              height="50px"
+              alt="image of node icon"
+              style={{animationDelay: "-12s"}}
+            />
+          </div>
         </div>
 
         <a
