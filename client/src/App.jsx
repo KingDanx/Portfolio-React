61f92e364f1469fe1782be15fd7b0fec28203864
Dan Prudhomme <dannyprud@yahoo.com>
2022-01-22 22:56:36 -0600

    items rotating around a fixed point

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Link, Route, useParams, useNavigate } from "react-router-dom";
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import AboutMe from "./components/pages/AboutMe/AboutMe";
 import Contact from "./components/pages/Contact/Contact";
 import Home from "./components/pages/Home/Home";
 import Projects from "./components/pages/Projects/Projects";
 
 const App = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <div>
+      <div>
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Button>Home</Button>
+          </Link>
+          <Link to="aboutme" style={{ textDecoration: "none" }}>
+            <Button>About Me</Button>
+          </Link>
+          <Link to="projects" style={{ textDecoration: "none" }}>
+            <Button>Projects</Button>
+          </Link>
+          <Link to="contact" style={{ textDecoration: "none" }}>
+            <Button>Contact</Button>
+          </Link>
+        </ButtonGroup>
+      </div>
 
-    return ( 
-        <div>Dan Prudhomme Jr
-<div>
-    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Link to="/" style={{textDecoration: 'none'}}><Button>Home</Button></Link>
-        <Link to="aboutme" style={{textDecoration: 'none'}}><Button>About Me</Button></Link>
-        <Link to="projects" style={{textDecoration: 'none'}}><Button>Projects</Button></Link>
-        <Link to="contact" style={{textDecoration: 'none'}}><Button>Contact</Button></Link>
-    </ButtonGroup>
-</div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="projects" element={<Projects />}></Route>
+        <Route path="aboutme" element={<AboutMe />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
-
-<Routes>
-          <Route
-            path="/"
-            element={<Home/>}
-          ></Route>
-          <Route
-            path="projects"
-            element={<Projects/>}
-          ></Route>
-          <Route
-            path="aboutme"
-            element={<AboutMe/>}
-          ></Route>
-          <Route
-            path="contact"
-            element={<Contact/>} 
-          ></Route>
-        </Routes> 
-        </div>
-     );
-}
- 
 export default App;
--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,19 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import resume from "../../../resume/Dan Prudhomme - dev Resume.docx";
-import Button from '@mui/material/Button';
+import dan from "../../../images/Dan-Prudhomme.jpg";
+import Button from "@mui/material/Button";
 import "./styles/Home.css";
-import { bgcolor } from '@mui/system';
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
 
 const Home = () => {
-
-    return ( 
-        <div>Home
-
-            <div>
-            <a href={resume} style={{textDecoration: 'none'}} download="Dan Prudhomme - Resume"> <Button variant="contained" sx={{borderRadius: "500px"}}>Lets Work Together</Button></a>
-            </div>
+  return (
+    <div>
+      Home
+      <h1>Dan Prudhomme Jr</h1>
+      <div>
+        <img src={dan} alt="image of Dan Prudhomme Jr" />
+        <div className="main">
+          <div className="item">
+            <img
+              src={dan}
+              height="50px"
+              style={{ borderRadius: "50%" }}
+              alt="image of Dan Prudhomme Jr"
+            />
+          </div>
+          <div className="item"><AirplanemodeActiveIcon/></div>
         </div>
-     );
-}
- 
+
+        <a
+          href={resume}
+          style={{ textDecoration: "none" }}
+          download="Dan Prudhomme - Resume"
+        >
+          {" "}
+          <Button
+            variant="contained"
+            sx={{ borderRadius: "500px", textTransform: "none" }}
+          >
+            Lets Work Together
+          </Button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
