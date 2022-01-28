e36672e1f7764c057bfa0a1f3769051fc79f12a8
Dan Prudhomme <dannyprud@yahoo.com>
2022-01-27 21:36:07 -0600

    Finish imputing data into mui tabs, may replace tab with different component later

--- client/src/components/pages/AboutMe/AboutMe.jsx
+++ client/src/components/pages/AboutMe/AboutMe.jsx
@@ -25,7 +25,14 @@ const AboutMe = () => {
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Hire Me Because…" value="1" />
             <Tab label="Passion For Programming" value="2" />
-            <Tab label="Item Three" value="3" />
+            <Tab
+              label="Three Words About My Professional Qualities"
+              value="3"
+            />
+            <Tab
+              label="Three Words That Describe The Best Role For Me"
+              value="4"
+            />
           </TabList>
         </Box>
         <TabPanel value="1">
@@ -46,37 +53,58 @@ const AboutMe = () => {
           that previously seemed impossible gives me an abundance of thrill,
           fulfillment, and motivation to continue.
         </TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="3">
+          {" "}
+          <p>
+            Persistent: When presented with a problem I do not give up until I
+            find some kind of solution. There are so many ways to solve problems
+            and I will break down my issue and attack from every angle until I
+            have a viable solution to my problem or until I make it a
+            non-problem.
+          </p>
+          <p>
+            Humble: I am not afraid of humility or being wrong. I enjoy
+            constructive criticism and consider the advice given to me. I
+            believe the moment we are right about everything we will no longer
+            be on the frontier of innovation.
+          </p>
+          <p>
+            {" "}
+            Philomath: I have a passion for learning. I believe receiving
+            mentorship is invaluable. I spend a minimum of three hours a day
+            listening or watching educational content, from programming to
+            astrophysics, to anthropology, and much more.
+          </p>
+        </TabPanel>
+        <TabPanel value="4">
+          <p>
+            {" "}
+            Challenging: I am a natural problem solver and I am ready and
+            willing to take on the complex task my team assigns to me.{" "}
+          </p>
+
+          <p>
+            {" "}
+            Meaningful: I want my work to be fulfilling. I want to develop
+            exceptional products that are easy to use, understand, and better
+            the lives of consumers.{" "}
+          </p>
+
+          <p>
+            {" "}
+            Growth: I want to be in a position where I can absorb knowledge from
+            my mentors and peers. I am eager to learn and grow as a developer to
+            make the best products I am capable of creating.
+          </p>
+        </TabPanel>
       </TabContext>
     </Box>
-    // <div style={themeStyles}>
-    //   <h2>Passion For Programming</h2>
-    //   <div>
+    // <div style={themeStyles}
 
     //   </div>
-    //   <h2>Three Words About My Professional Qualities</h2>
+    //   <h2></h2>
     //   <div>
-    //     Persistent: When presented with a problem I do not give up until I find
-    //     some kind of solution. There are so many ways to solve problems and I
-    //     will break down my issue and attack from every angle until I have a
-    //     viable solution to my problem or until I make it a non-problem. Humble:
-    //     I am not afraid of humility or being wrong. I enjoy constructive
-    //     criticism and consider the advice given to me. I believe the moment we
-    //     are right about everything we will no longer be on the frontier of
-    //     innovation. Philomath: I have a passion for learning. I believe
-    //     receiving mentorship is invaluable. I spend a minimum of three hours a
-    //     day listening or watching educational content, from programming to
-    //     astrophysics, to anthropology, and much more.
-    //   </div>
-    //   <h2>Three Words That Describe The Best Role For Me</h2>
-    //   <div>
-    //     Challenging: I am a natural problem solver and I am ready and willing to
-    //     take on the complex task my team assigns to me. Meaningful: I want my
-    //     work to be fulfilling. I want to develop exceptional products that are
-    //     easy to use, understand, and better the lives of consumers. Growth: I
-    //     want to be in a position where I can absorb knowledge from my mentors
-    //     and peers. I am eager to learn and grow as a developer to make the best
-    //     products I am capable of creating.
+
     //   </div>
     // </div>
   );
