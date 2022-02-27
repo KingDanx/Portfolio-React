d596deab734d4ea53330e0164ebe81ed046e144c
Dan Prudhomme <dannyprud@yahoo.com>
2022-02-27 13:32:24 -0600

    Create styles folder for projects page, create Projects.css with class to organize page.  Add iframe that will hold video of projects.

--- client/src/components/pages/Projects/Projects.jsx
+++ client/src/components/pages/Projects/Projects.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ProjectAccordion from './components/Accordion.jsx';
+import './styles/Projects.css';
 
 const Projects = () => {
     return ( 
-        <div>Projects
+        <div className='project-split'>
             <div>
                 <ProjectAccordion/>
+            </div>
+            <div>
+                <iframe></iframe>
             </div>
         </div>
      );
